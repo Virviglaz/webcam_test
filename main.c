49e148f60bbeb1f9303443177666208bbc88fac2
Pavel Nadein <pavelnadein@gmail.com>
2019-08-11 03:40:13 -0700

    added ioctl

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/videodev2.h>
 #include "framebuffer.h"
 
 int main() {
@@ -10,6 +13,7 @@ int main() {
 	int cam_fd;
 	float data[size];
 	int res = 0;
+	struct v4l2_capability capability;
 	
 	printf("Opening framebuffer: %s\n", FrameBufferInit(NULL, 0));
 	
@@ -21,9 +25,19 @@ int main() {
 		goto free_fb;
 	}
 
+	if (ioctl(cam_fd, VIDIOC_QUERYCAP, &capability)) {
+		res = errno;
+		printf("Failed to do IOCTL %s, error: %s\n",
+			device, strerror(res));	
+		goto close_cam;
+	}
+
+	printf("Camera name: %s\n", capability.card);
+
 	//if (read(data, sizeof(data[0]), size, camLive) != size)
 		//printf("Error reading device %s\n", device);
 
+close_cam:
 	close(cam_fd);
 	
 free_fb:
